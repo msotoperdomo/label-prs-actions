1d61077de2424c3c581baadf8a512e83c545fd5f
Maria Soto <maria.perdomo@lightspeedhq.com>
2020-02-17 16:20:09 -0500

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+
+label_pr_when_approved() {
+    echo "This action will label a PR when approved"
+}
+
+remove_label_pr() {
+    echo "This action will remove the label of a PR"
+}
+
+
+PR_ACTION="$1"
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_EVENT_PATH" ]]; then
+  echo "Set the GITHUB_EVENT_PATH env variable."
+  exit 1
+fi
+
+if [[ -z "$PR_ACTION" ]]; then
+  echo "Set the PR_ACTION env variable."
+  exit 1
+fi
+
+echo "PR ACTION: ${PR_ACTION}"
+
+case "${PR_ACTION}" in
+    label_pr_when_approved)
+        label_pr_when_approved $@;;
+    remove_label_pr)
+        remove_label_pr $@;;
+esac
