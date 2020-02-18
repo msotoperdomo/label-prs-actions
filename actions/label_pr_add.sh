d9dfb33107444b7ab73ab44e81b6a2e72cfd8e69
Maria Soto <maria.perdomo@lightspeedhq.com>
2020-02-18 14:52:29 -0500

    Clean up

--- actions/label_pr_add.sh
+++ actions/label_pr_add.sh
@@ -2,19 +2,23 @@
 set -e
 
 label_pr_add() {
-    echo "This action will add a label of a PR"
+
 
     if [[ -z "${ADD_LABEL}" ]]; then
       echo "Set the ADD_LABEL env variable."
       exit 1
     fi
 
-    local result=$(curl -sSL \
+    echo "This action will add the label ${ADD_LABEL} to the PR ${number}"
+
+    local response=$(curl -sSL \
         -H "${AUTH_HEADER}" \
         -H "${API_HEADER}" \
         -X POST \
         -H "Content-Type: application/json" \
         -d "{\"labels\":[\"${ADD_LABEL}\"]}" \
         "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels")
+
+    echo "response: ${response}"
 
 }
--- actions/label_pr_remove.sh
+++ actions/label_pr_remove.sh
@@ -2,19 +2,20 @@
 set -e
 
 label_pr_remove() {
-    echo "This action will remove the label of a PR"
 
     if [[ -z "${REMOVE_LABEL}" ]]; then
       echo "Set the REMOVE_LABEL env variable."
       exit 1
     fi
 
-    curl -sSL \
+    echo "This action will remove the label ${REMOVE_LABEL} of the PR ${number}, if it exists."
+
+    local status=$(curl -sSL \
       -H "${AUTH_HEADER}" \
       -H "${API_HEADER}" \
       -X DELETE \
-      "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels/${REMOVE_LABEL}"
+      "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels/${REMOVE_LABEL}")
 
-      time=$(date)
+     echo "response: ${status}"
 
 }
